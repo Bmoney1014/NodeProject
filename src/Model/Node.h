20e4a35a86948f0d6cf2c3c61ad68b623b3cc06a
Brock Hostetter <bstetterslc@gmail.com>
2016-01-27 09:33:46 -0700

    addedNodeProject to repo

---
+++ src/Model/Node.h
@@ -0,0 +1,17 @@
+/*
+ * Node.h
+ *
+ *  Created on: Jan 27, 2016
+ *      Author: bhos1889
+ */
+
+#ifndef NODE_H_
+#define NODE_H_
+
+class Node {
+public:
+	Node();
+	virtual ~Node();
+};
+
+#endif /* NODE_H_ */
